edb6f77aee04afa914e730b97374788c88023970
Trakivymd <104880686+Trakivymd@users.noreply.github.com>
2022-05-05 21:54:20 +0630

    Update note.py

--- note.py
+++ note.py
@@ -1,1 +1,2 @@
 font style- https://raw.githubusercontent.com/HeaTTheatR/KivyMD-data/master/gallery/kivymddoc/md-label-font-style.gif
+icon - https://github.com/HeaTTheatR/KivyMD/blob/master/kivymd/icon_definitions.py
